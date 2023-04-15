d9fb57de7e0b1a6d01e3b0514a85faa5bccc963d
unknown <habtamuararsie@yahoo.com>
2023-04-15 16:27:50 +0300

    page size

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,8 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int)\
-        -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """[summary]
 
     Args:
