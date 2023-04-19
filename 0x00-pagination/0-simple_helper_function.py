1113ffb23428b6b36eab15b99db76c5e62150971
unknown <habtamuararsie@yahoo.com>
2023-04-19 19:16:59 +0300

    pagination update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,13 +6,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Args:
-        page (int): the current page
-        page_size (int): the size of items in a page
-        first_index: calculate the first index
-        last_index: calculate the last index 
-        indexOfrange: return first index and last index 
-    Returns:
-        (tuple): a row/tuple of the start and end index of the given page
-    """
+   return a tuple of size two containing a start index and an end index
+   Page numbers are 1-indexed, i.e. the first page is page 1.
+   """
     return ((page - 1) * page_size, page * page_size)
