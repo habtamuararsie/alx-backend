f6d30b9b76862b70d4def7283e79c0df8e85987f
unknown <habtamuararsie@yahoo.com>
2023-04-19 18:34:00 +0300

    pagination update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,18 +3,10 @@
 import csv
 import math
 from typing import List
-# index_range = __import__('0-simple_helper_function').index_range
-
-
-def index_range(self, page: int, page_size: int) -> tuple:
-    """
-        return a tuple of size two containing a start index and an end index
-        Page numbers are 1-indexed, i.e. the first page is page 1.
-        """
-    return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
+
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
@@ -33,7 +25,26 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+
+@staticmethod
+def index_range(page: int, page_size: int) -> tuple:
+        """
+        Args:
+        page (int): the current page
+        page_size (int): the size of items in a page
+        first_index: calculate the first index
+        last_index: calculate the last index
+        indexOfrange: return first index and last index
+        Returns:
+        (tuple): a row/tuple of the start and end index of the given page
+        """
+
+        first_index = (page - 1) * page_size
+        last_index = first_index + page_size
+        indexOfrange = first_index, last_index
+        return indexOfrange
+
+def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """[summary]
         Args:
             page (int, optional): [description]. Defaults to 1.
