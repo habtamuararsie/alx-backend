caafff8a3e75cb181a1b40c6571522fcd437dc44
unknown <habtamuararsie@yahoo.com>
2023-04-19 19:44:45 +0300

    caching update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,11 +6,11 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    
+
     """
     FIFOCache
     """
-
+       
     def __init__(self):
         """
         Initialize  the cache
@@ -20,13 +20,11 @@ class FIFOCache(BaseCaching):
         self.keys = []
 
     def put(self, key, item):
-
         """
         assign a key to value to cache if limit not reached
         otherwise delete the first item
         """
         if key and item:
-            return
             if len(self.keys) >= self.MAX_ITEMS:
                 if key not in self.keys:
                     print(f"DISCARD: {self.keys[0]}")
@@ -42,4 +40,3 @@ class FIFOCache(BaseCaching):
         if key not in self.keys:
             return None
         return self.cache_data[key]
-
