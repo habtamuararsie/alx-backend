5ddb2f40321480a286995b6d0c57b916f7754f64
unknown <habtamuararsie@yahoo.com>
2023-04-19 19:31:39 +0300

    pagination update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,7 +27,7 @@ class Server:
         return self.__dataset
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
     """
    return a tuple of size two containing a start index and an end index
    Page numbers are 1-indexed, i.e. the first page is page 1.
