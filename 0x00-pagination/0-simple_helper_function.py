92cd0e74bf755970da898466c44d9d8d24a9bb69
unknown <habtamuararsie@yahoo.com>
2023-04-19 19:00:05 +0300

    pagination update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Task 0 genrate index range"""
+from typing import Tuple
 
-
-def index_range(self, page: int, page_size: int) -> tuple:
+def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index and an end index
     Page numbers are 1-indexed, i.e. the first page is page 1.
